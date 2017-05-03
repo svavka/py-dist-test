16e2a2847721720b24ac03d60961ca3964248451
svavka <svavka@gmail.com>
2017-05-03 21:45:12 +0300

    add web using

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 from setuptools import setup, find_packages 
 from os.path import join, dirname 
+import helloworld
+
 
 setup(
     name='helloworld',
-    version='1.0',
+    version=helloworld.__version__,
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.rst')).read(),
     entry_points={
         'console_scripts':
-            ['hellowolrd=helloworld.core:print_message']
-    }
+            [
+                'hellowolrd=helloworld.core:print_message',
+                'serve=helloworld.web:run_server'
+            ]
+    },
+    install_requires=[
+        'Flask'
+    ],
+    include_package_data=True
 )
